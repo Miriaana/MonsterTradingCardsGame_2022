c3546363f21335c3622c10c3f56bdbddedbdf108
Miriaana <7h.miriam@gmail.com>
2022-10-05 11:26:17 +0200

    add first TestProject
    oops, that doesn't belong in this branch

---
+++ MonsterTradingCardsGame.BL.Test/GameHandlerTest.cs
@@ -0,0 +1,16 @@
+namespace MonsterTradingCardsGame.BL.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
